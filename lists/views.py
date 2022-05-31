4b236b09b8f413d7991833e54824b4768434a72b
Daydreumer <2765129970@qq.com>
2022-05-31 13:39:06 +0800

    refact to home.html, add new url.

--- lists/views.py
+++ lists/views.py
@@ -4,12 +4,16 @@ from lists.models import Item
 
 # Create your views here.
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
+    # if request.method == 'POST':
+    #     Item.objects.create(text=request.POST['item_text'])
+    #     return redirect('/lists/the-only-list-in-the-world/')
     # return HttpResponse('<html><title>To-Do lists</title></html>')
     # items = Item.objects.all()
     return render(request, 'home.html')
+    
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
     
 def view_list(request):
     items = Item.objects.all()
